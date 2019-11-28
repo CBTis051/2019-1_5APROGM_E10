540e592f28ee960fa62c0f19db8696c76a4e060a
Jose Carlos Cervantes Aguirre <al17190@cbtis051.edu.mx>
2019-11-28 15:31:16 -0700

    ajustes en las respuesta del formato JSON

--- app_al17190.js
+++ app_al17190.js
@@ -2,7 +2,7 @@ var http = require('http');
 http.createServer(function (req, res) {
     res.writeHead(200, {'content-Type': 'application/json; charset=utf-8'});
         var objeto = {
-            mensaje : 'Hola mundo Node JS Repo GIT - Jose Carlos Cervantes Aguirre\n'
+            mensaje : 'Hola mundo Node JS Repo GIT - Jose Carlos Cervantes Aguirre \n'
         };
 
         var json = JSON.stringify(objeto);
