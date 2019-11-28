7fd20895cecbd117a93cc3ca1a551e82da871153
Jose Carlos Cervantes Aguirre <al17190@cbtis051.edu.mx>
2019-11-28 15:09:35 -0700

    “”

--- app_al17190.js
+++ app_al17190.js
@@ -1,7 +1,13 @@
 var http = require('http');
 http.createServer(function (req, res) {
     res.writeHead(200, {'content-Type': 'application/json; charset=utf-8'});
+        var objeto = {
+            mensaje : 'Hola mundo Node JS Repo GIT - Jose Carlos Cervantes Aguirre\n'
+        };
 
-    res.end('Hola mundo Node JS Repo GIT - Jose Carlos Cervantes Aguirre\n');
+        var json = JSON.stringify(objeto);
+    res.end();
+
+
 }).listen(8081);
 console.log('Server running on port 8081');
